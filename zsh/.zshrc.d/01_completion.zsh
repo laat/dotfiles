c1b19d41ce215066982bc2128dffd6fb8ec48c98
Sigurd Fosseng <sigurd@fosseng.net>
2015-10-04 18:09:08 +0200

    basic completion

---
+++ zsh/.zshrc.d/01_completion.zsh
@@ -0,0 +1,8 @@
+autoload -U compinit && compinit
+setopt completealiases
+
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:kill:*'   force-list always
+
+zstyle ':completion:*:*:killall:*' menu yes select
+zstyle ':completion:*:killall:*'   force-list always
