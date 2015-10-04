2f206c2b504a6905428afc347b1a4ff621466302
Sigurd Fosseng <sigurd@fosseng.net>
2015-10-04 15:28:34 +0200

    Fixed a bug in my prompt

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -1,4 +1,5 @@
+autoload -U colors && colors
 NEWLINE=$'\n'
 #clean prompt with host
 if [ $UID -eq 0 ]; then NCOLOR="red"; else NCOLOR="white"; fi
-PROMPT='%{$fg[$NCOLOR]%}%B%n%b@$FG[145]%m%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} ${NEWLINE}%(!.#.$) '
+PROMPT="%{$fg[$NCOLOR]%}%B%n%b@$FG[145]%m%{$reset_color%}:%{$fg[blue]%}%B%c/%b%{$reset_color%} ${NEWLINE}%(!.#.$) "
