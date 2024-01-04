fe6a47d840af158f77dabc28e4ade39756a16a37
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-04 20:26:31 +0100

    setup new mac

---
+++ zsh/.zshrc.d/10_antidote.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+ANTIDOTE_FOLDER="$HOME/.antidote"
+zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
+
+if [ ! -d "$ANTIDOTE_FOLDER" ] ; then
+    git clone https://github.com/mattmc3/antidote.git $ANTIDOTE_FOLDER
+    cat <<EOF >> ${zsh_plugins}.txt
+zdharma-continuum/fast-syntax-highlighting kind:defer
+ohmyzsh/ohmyzsh path:plugins/thefuck kind:defer
+EOF
+fi
+
+if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+  (
+    source "$ANTIDOTE_FOLDER/antidote.zsh"
+    antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
+  )
+fi
+source ${zsh_plugins}.zsh
+unset ANTIDOTE_FOLDER
