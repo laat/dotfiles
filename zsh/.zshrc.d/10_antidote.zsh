8fb36ce33259f9c668152ea6b42ec24196059284
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-05 18:41:38 +0100

    cfg

--- zsh/.zshrc.d/10_antidote.zsh
+++ zsh/.zshrc.d/10_antidote.zsh
@@ -1,16 +1,17 @@
 #!/bin/zsh
 ANTIDOTE_FOLDER="$HOME/.antidote"
+ZSH_CACHE_DIR="$HOME/.cache/oh-my-zsh"
 zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
 
 if [ ! -d "$ANTIDOTE_FOLDER" ] ; then
     git clone https://github.com/mattmc3/antidote.git $ANTIDOTE_FOLDER
+    mkdir -p "$HOME/.cache/oh-my-zsh"
 fi
 
 if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
   (
     source "$ANTIDOTE_FOLDER/antidote.zsh"
     antidote bundle <${zsh_plugins}.txt >${zsh_plugins}.zsh
-    mkdir -p "~/.cache/oh-my-zsh"
   )
 fi
 source ${zsh_plugins}.zsh
