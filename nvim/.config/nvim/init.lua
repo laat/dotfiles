7dc5ad34ea9a589d8e4c8786179686f5ab8c10ad
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-06 21:08:18 +0100

    cfg

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -286,22 +286,20 @@ require('lazy').setup({
     'nvim-tree/nvim-tree.lua',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      local function my_on_attach(bufnr)
-        local api = require "nvim-tree.api"
-
-        local function opts(desc)
-          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-        end
-
-        -- default mappings
-        api.config.mappings.default_on_attach(bufnr)
-
-        -- custom mappings
-        vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
-        vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
-      end
       require("nvim-tree").setup({
-        on_attach = my_on_attach
+        on_attach = function (bufnr)
+          local api = require "nvim-tree.api"
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- default mappings
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- custom mappings
+          vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+          vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+        end
       })
     end,
     keys = {
