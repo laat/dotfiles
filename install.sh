9fdfaedcf400fc66dc448fbb29c2f901be3fb8d8
Sigurd Fosseng <sigurd@fosseng.net>
2024-01-18 02:47:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,15 @@
 
 sudo apt-get -q -y update
 sudo apt-get install -q -y --no-install-recommends stow
+pip install thefuck
 
 function stow() {
   /usr/bin/stow -t "$HOME" --restow --no-folding "$@" \
     2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
 }
 
-rm ~/.profile ~/.bashrc
+rm -f ~/.profile ~/.bashrc ~/.zprofile ~/.zshrc
 
 stow stow
 stow --stow skel
-stow --stow git pnpm npm codespaces
+stow --stow git pnpm npm codespaces zsh
